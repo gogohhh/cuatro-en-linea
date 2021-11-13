c36c231289529647a79a8263e8c49d04fa58bc79
gogohhh <gogo_hhh_d@hotmail.com>
2021-11-13 16:34:25 -0600

    Test para render

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,12 @@
 import Cell from './Cell.js';
 
-const Row = ({ row, index }) => {
-
-    return (
-      <tr>
-        {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} index={index} />)} {/* Pasamos las props de la celda*/}
-        {/* Como en el componente principal es una tabla recorremos las filas con los indeces de las celdas */}
-      </tr>
-    );
+const Row = ({row,index}) => {
+  return (
+    <tr>
+      {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} index={index} />)} {/* Pasamos las props de la celda*/}
+      {/* Como en el componente principal es una tabla recorremos las filas con los indeces de las celdas */}
+    </tr>
+  );
   };
 
 export default Row;
--- src/__test__/App.js/App.test.js
+++ src/__test__/App.js/App.test.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import {mount} from 'enzyme';
-import App from '../App';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import App from '../../App.js';
 
 
+test('Test de rendereado de contenido', ()=>{
+    const note = {
+        content: 'Este es un test',
+        important: true
+    }
+
+    const component = render(App);
+    console.log(component);
+});
